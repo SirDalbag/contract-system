4f6b82ee8051b670831df97dd9d6e9857b150a0f
SirDalbag <sir.dalbag@gmail.com>
2024-03-13 17:21:23 +0500

    update

--- backend/django_app/urls.py
+++ backend/django_app/urls.py
@@ -3,10 +3,13 @@ from django_app import views, models, serializers
 
 urlpatterns = [
     path("api/", views.api),
-    path('api/contracts/', views.objects, {'model': models.Contract, 'serializer': serializers.ContractSerializer}),
-    path('api/contract/<int:id>/', views.objects, {'model': models.Contract, 'serializer': serializers.ContractSerializer}),
-    path('api/agents/', views.objects, {'model': models.Agent, 'serializer': serializers.AgentSerializer}),
-    path('api/agent/<int:id>/', views.objects, {'model': models.Agent, 'serializer': serializers.AgentSerializer}),
-    path('api/comments/', views.objects, {'model': models.Comment, 'serializer': serializers.CommentSerializer}),
-    path('api/comment/<int:id>/', views.objects, {'model': models.Comment, 'serializer': serializers.CommentSerializer}),
+    path('api/get/contracts/', views.get_objects_or_object, {'model': models.Contract, 'serializer': serializers.ContractSerializer}),
+    path('api/get/contract/<int:id>/', views.get_objects_or_object, {'model': models.Contract, 'serializer': serializers.ContractSerializer}),
+    path('api/post/contract/', views.post_object, {'serializer': serializers.ContractSerializer}),
+    path('api/get/agents/', views.get_objects_or_object, {'model': models.Agent, 'serializer': serializers.AgentSerializer}),
+    path('api/get/agent/<int:id>/', views.get_objects_or_object, {'model': models.Agent, 'serializer': serializers.AgentSerializer}),
+    path('api/post/agent/', views.post_object, {'serializer': serializers.AgentSerializer}),
+    path('api/get/comments/', views.get_objects_or_object, {'model': models.Comment, 'serializer': serializers.CommentSerializer}),
+    path('api/get/comment/<int:id>/', views.get_objects_or_object, {'model': models.Comment, 'serializer': serializers.CommentSerializer}),
+    path('api/post/comment/', views.post_object, {'serializer': serializers.CommentSerializer}),
 ]
--- backend/django_app/views.py
+++ backend/django_app/views.py
@@ -6,9 +6,21 @@ from django_app import utils
 @api_view(http_method_names=["GET", "POST"])
 @permission_classes([AllowAny])
 def api(request):
-    return Response(data={"message": "OK"})
+    if request.method == "GET":
+        return Response(data={"message": "OK"})
+    elif request.method == "POST":
+        return Response(data={"message": request.data})
 
 @api_view(http_method_names=["GET"])
 @permission_classes([AllowAny])
-def objects(request, model, serializer, id: int = None):
+def get_objects_or_object(request, model, serializer, id: int = None):
     return Response(data={"message": utils.serialization(model, serializer, id=id) if id else utils.serialization(model, serializer, sort=request.GET.get("sort", None))})
+
+@api_view(http_method_names=["POST"])
+@permission_classes([AllowAny])
+def post_object(request, serializer):
+    serializer = serializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
