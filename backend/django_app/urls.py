13cda639159d4b6a45475cd8ca9f188d8e19b967
SirDalbag <sir.dalbag@gmail.com>
2024-03-13 22:03:59 +0500

    update

--- backend/django_app/urls.py
+++ backend/django_app/urls.py
@@ -1,16 +1,70 @@
 from django.urls import path
 from django_app import views, models, serializers
+from django.contrib.auth.models import User
 
 urlpatterns = [
     path("api/", views.api),
-    path('api/get/contracts/', views.get_objects_or_object, {'model': models.Contract, 'serializer': serializers.ContractSerializer}),
-    path('api/get/contract/<int:id>/', views.get_objects_or_object, {'model': models.Contract, 'serializer': serializers.ContractSerializer}),
-    path('api/post/contract/', views.post_object, {'serializer': serializers.ContractSerializer}),
-    path('api/get/agents/', views.get_objects_or_object, {'model': models.Agent, 'serializer': serializers.AgentSerializer}),
-    path('api/get/agent/<int:id>/', views.get_objects_or_object, {'model': models.Agent, 'serializer': serializers.AgentSerializer}),
-    path('api/post/agent/', views.post_object, {'serializer': serializers.AgentSerializer}),
-    path('api/get/comments/', views.get_objects_or_object, {'model': models.Comment, 'serializer': serializers.CommentSerializer}),
-    path('api/get/comment/<int:id>/', views.get_objects_or_object, {'model': models.Comment, 'serializer': serializers.CommentSerializer}),
-    path('api/post/comment/', views.post_object, {'serializer': serializers.CommentSerializer}),
-    path('api/get/contracts/author/<int:id>', views.get_contracts_by_author),
+    path(
+        "api/get/contracts/",
+        views.get_objects_or_object,
+        {"model": models.Contract, "serializer": serializers.ContractSerializer},
+    ),
+    path(
+        "api/get/contract/<int:id>/",
+        views.get_objects_or_object,
+        {"model": models.Contract, "serializer": serializers.ContractSerializer},
+    ),
+    path(
+        "api/get/contracts/author/<int:id>",
+        views.get_objects_by_field,
+        {
+            "model": models.Contract,
+            "serializer": serializers.ContractSerializer,
+            "field": {"author": User},
+        },
+    ),
+    path(
+        "api/get/contracts/agent/<int:id>",
+        views.get_objects_by_field,
+        {
+            "model": models.Contract,
+            "serializer": serializers.ContractSerializer,
+            "field": {"agent_id": models.Agent},
+        },
+    ),
+    path(
+        "api/post/contract/",
+        views.post_object,
+        {"serializer": serializers.ContractSerializer},
+    ),
+    path(
+        "api/get/agents/",
+        views.get_objects_or_object,
+        {"model": models.Agent, "serializer": serializers.AgentSerializer},
+    ),
+    path(
+        "api/get/agent/<int:id>/",
+        views.get_objects_or_object,
+        {"model": models.Agent, "serializer": serializers.AgentSerializer},
+    ),
+    path(
+        "api/post/agent/",
+        views.post_object,
+        {"serializer": serializers.AgentSerializer},
+    ),
+    path(
+        "api/get/comments/",
+        views.get_objects_or_object,
+        {"model": models.Comment, "serializer": serializers.CommentSerializer},
+    ),
+    path(
+        "api/get/comment/<int:id>/",
+        views.get_objects_or_object,
+        {"model": models.Comment, "serializer": serializers.CommentSerializer},
+    ),
+    path(
+        "api/post/comment/",
+        views.post_object,
+        {"serializer": serializers.CommentSerializer},
+    ),
 ]
--- backend/django_app/views.py
+++ backend/django_app/views.py
@@ -1,8 +1,8 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from django.contrib.auth.models import User
-from django_app import models, serializers, utils
+from django_app import utils
+
 
 @api_view(http_method_names=["GET", "POST"])
 @permission_classes([AllowAny])
@@ -12,28 +12,50 @@ def api(request):
     elif request.method == "POST":
         return Response(data={"message": request.data})
 
+
 @api_view(http_method_names=["GET"])
 @permission_classes([AllowAny])
 def get_objects_or_object(request, model, serializer, id: int = None):
-    return Response(data={"data": utils.serialization(model, serializer, id=id) if id else utils.serialization(model, serializer, sort=request.GET.get("sort", None))})
+    try:
+        return Response(
+            data={
+                "data": (
+                    utils.serialization(model, serializer, id=id)
+                    if id
+                    else utils.serialization(
+                        model, serializer, sort=request.GET.get("sort", None)
+                    )
+                )
+            }
+        )
+    except Exception as error:
+        return Response(data={"message": str(error)})
+
 
 @api_view(http_method_names=["POST"])
 @permission_classes([AllowAny])
 def post_object(request, serializer):
-    serializer = serializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=201)
-    return Response(serializer.errors, status=400)
+    try:
+        serializer = serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    except Exception as error:
+        return Response(data={"message": str(error)})
+
 
 @api_view(http_method_names=["GET"])
 @permission_classes([AllowAny])
-def get_contracts_by_author(request, id: int = None):
+def get_objects_by_field(request, model, serializer, field, id: int = None):
     try:
-        author = User.objects.get(id=id)
-        contracts = models.Contract.objects.filter(author=author)
-        serializer = serializers.ContractSerializer(contracts, many=True)
-        total_count = contracts.count()
-        return Response(data={"data": serializer.data, "total_count": total_count})
+        key = list(field.keys())[0]
+        value = field[key].objects.get(id=id)
+        objects = utils.serialization(
+            model,
+            serializer,
+            **{key: value},
+        )
+        return Response(data={"data": objects, "total_count": len(objects)})
     except Exception as error:
-        return Response(data={"data": "", "total_count": 0, "message": str(error)}, status=400)
+        return Response(data={"message": str(error)})
